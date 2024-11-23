9d2f4a2fa07e5297d7bee99008b73e358aa2f644
DheerajRay-01 <dheerajray2003@gmail.com>
2024-11-23 14:03:24 +0530

    add feature> support.jsx

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -4,20 +4,12 @@ import React from 'react'
 function Support() {
 
   return (
-    <div className='w-[90%] md:w-[60%] lg:w-[50%] m-auto h-fit p-6 rounded-xl bg-gray-800 shadow-lg mt-10 mb-10'>
+    <div className='w-[90%] md:w-[60%] lg:w-[50%] m-auto h-fit p-6 rounded-xl bg-gray-800 shadow-lg mt-7 mb-7'>
         <h1 className='text-3xl font-bold text-white mb-4'>Contact Support</h1>
         <p className='text-lg text-gray-300 mb-6'>
             If you have any issues or want to report a bug, please create an issue using the button below.
         </p>
         <div className='flex flex-col gap-5 items-center'>
-        <button
-                className='px-6 py-3 rounded-md text-white bg-blue-600 hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-400'
-                onClick={() => window.open("https://github.com/DheerajRay-01/myNote-appwrite/discussions/1", "_blank")}
-                aria-label="Create an issue on GitHub"
-            >
-                Ask a Question
-            </button>
-
             <button
                 className='px-6 py-3 rounded-md text-white bg-blue-600 hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-400'
                 onClick={() => window.open("https://github.com/DheerajRay-01/myNote-appwrite/issues/new", "_blank")}
